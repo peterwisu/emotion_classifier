ce3f8064469df6ef01dece5817fa99c9575e668d
Wish Suharitdamrong <peterwisu@gmail.com>
2023-05-19 18:51:47 +0100

    initialise

---
+++ main.py
@@ -0,0 +1,69 @@
+import gradio as gr
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model import EmoClassifier
+import typing
+import logging
+
+
+logging.basicConfig(filename="log_file.log",
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    format='%(asctime)s ,%(levelname)s:, user_input: %(user_input)s, model_prediction: %(model_prediction)s, score: %(score)s',
+                    
+                    )
+
+# initialise predictor
+emo_predictor = EmoClassifier()
+
+# # create UI
+# demo = gr.Interface(
+#         fn=emo_predictor.predict_emo,
+#         inputs=gr.Textbox(label="Ask a question"),
+#         outputs=[gr.Textbox(label="text"),gr.Textbox(label="Answer"),gr.Number(label="Score")],
+#         allow_flagging="never",
+#         )
+
+# initialise server 
+app = FastAPI()
+
+# request object
+class Request(BaseModel):
+
+    text:  str
+
+# returning object
+class Result(BaseModel):
+
+    text: str
+    label: str
+    score: float
+
+# response object
+class Response(BaseModel):
+
+    results: typing.List[Result] 
+
+
+
+# API endpoint for prediction
+@app.post("/predict", response_model=Response)
+async def predict_api(request: Request):
+
+    results =  emo_predictor.predict_emo(request.text)
+    
+    logging.info("Logging additional Information", extra={'user_input': results[0], 'model_prediction': results[1], 'score': results[2]})
+
+    return Response(
+
+            results=[
+                        Result(text=results[0],label=results[1],score=results[2])
+                        
+                    ]
+
+            )
+
+# # mount UI on the server
+# app = gr.mount_gradio_app(app, demo, path="/")
+
+
---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+import transformers
+from transformers import BertTokenizer, BertModel, BertConfig , BertForSequenceClassification
+import numpy as np
+
+
+
+class EmoClassifier:
+
+
+    def __init__(self) -> None:
+
+        self.tokenizer = BertTokenizer.from_pretrained('./ckpt/bert_base_uncased_tokenizer/')
+        self.model = BertForSequenceClassification.from_pretrained('./ckpt/bert_ex4/')
+        self.model.eval()
+        self.labels = ['anger','confusion', 'curiosity', 'desire', 'digust', 'embarrassment', 'fear', 'joy', 'love', 'neutral', 'optimism', 'pride', 'sadness', 'surprise']
+
+
+    def predict_emo(self,text):
+
+        input = self.tokenizer.encode_plus(text,
+                                      None,
+                                      add_special_tokens=True,
+                                      max_length=128,
+                                      pad_to_max_length=True,
+                                      return_token_type_ids=True,
+                                      return_tensors='pt')
+
+        logits = self.model(**input)['logits']
+        proba = torch.nn.functional.softmax(logits, dim=1).detach().numpy()
+        idx = np.argmax(proba) 
+        pred = self.labels[idx]
+
+        return (text, pred, np.max(proba))
+
+
+
+
